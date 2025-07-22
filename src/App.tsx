0d78aa56dc51d197f3fe315e7586346f752c1016
palomaprates <paloma.prates108@gmail.com>
2025-07-22 16:49:22 +0100

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,39 @@
-import './App.css'
-import { useRef} from 'react'
-import Banner from './components/Banner'
-import Header from './components/Header'
-import Products from './components/Products'
-import LilaFooter from './components/LilaFooter'
-import SobreNos from './components/SobreNos'
-import SubProducts from './components/SubProducts'
-import InfosLila from './components/InfosLila'
-import Navbar from './components/LilaMenu'
-import Test from './components/LilaMenu'
-// import SobreNos from './components/SobreNos'
-
-// type ProductData = {
-//   title: string;
-//   image: string;
-//   description: string;
-// };
+import "./App.css";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import LilaFooter from "./components/LilaFooter";
+import SobreNos from "./components/SobreNos";
+import InfosLila from "./components/InfosLila";
 
 function App() {
-
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [selectedProduct, setSelectedProduct] = useState<ProductData | null>(null);
-
-  // const handleOpenModal = (product: ProductData) => {
-  //   setSelectedProduct(product);
-  //   setIsModalOpen(true);
-  // };
-  // const handleCloseModal = () => {
-  //   setIsModalOpen(false);
-  //   setSelectedProduct(null);
-  // };
-
-  const sobreRef = useRef<HTMLDivElement>(null);
-  const localizacaoRef = useRef<HTMLDivElement>(null);
-  const produtosRef = useRef<HTMLDivElement>(null);
-
   return (
-    <div className='min-h-screen w-screen'>
-      <div className='flex-grow'>
-    <Header />
-    <Banner />
-      {/* <Navbar
-        scrollToSobre={() => sobreRef.current?.scrollIntoView({ behavior: "smooth" })}
-        scrollToLocalizacao={() => localizacaoRef.current?.scrollIntoView({ behavior: "smooth" })}
-        scrollToProdutos={() => produtosRef.current?.scrollIntoView({ behavior: "smooth" })}
-        /> */}
-        <Test />
-      <InfosLila />
-    <div className='flex gap-7 justify-center'>
-      <Products />
-    </div>
-        <div ref={sobreRef} className='flex justify-center'>
-        <SobreNos/>
+    <div className="min-h-screen w-full overflow-hidden">
+      <div className="flex-grow flex flex-col items-center">
+        <Header />
+        <Banner />
+        <div className="max-w-[2000px] flex flex-col items-center">
+          <div className="max-w-[1300px]">
+            <InfosLila />
+            {/* <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3109.5751090566646!2d-9.3903429!3d38.7963738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1edac01052f76d%3A0x5d52a53447ad1add!2sArco%20do%20Terreirinho%201%2C%202710-623%20Sintra!5e0!3m2!1spt-PT!2spt!4v1752921389932!5m2!1spt-PT!2spt"
+              width="600"
+              height="450"
+              loading="lazy"
+            ></iframe> */}
+            <div id="produtos" className="flex gap-7 justify-center">
+              <Products />
+            </div>
+            <div id="sobre" className="flex justify-center">
+              <SobreNos />
+            </div>
+          </div>
         </div>
-        <SubProducts />
-      <LilaFooter/>
+        <div id="contacto" className="h-20 w-full bg-[#93c3cd]"></div>
+        <LilaFooter />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
