7433949c2cc68f8001533258496f3eb5b10c1981
palomaprates <paloma.prates108@gmail.com>
2025-07-22 15:42:34 +0100

    modify banner img

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,12 @@
-import lilabanner from '../assets/bannerlila.png'
+import lilabanner from "../assets/bannerlila.jpg";
 
 export default function Banner() {
-    
   return (
-    <div className="relative h-96 w-full">
+    <div className="relative h-96 w-full mt-14">
       <img
         src={lilabanner}
         alt="Banner Lila"
-        className="absolute inset-0 w-full h-full object-cover opacity-50"
+        className="absolute inset-0 w-full h-full object-cover opacity-50 "
       />
       <div className="absolute inset-0 bg-black opacity-55" />
       <div className="relative z-10 flex flex-col justify-center items-center text-white text-center h-full px-4">
