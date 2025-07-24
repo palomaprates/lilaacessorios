80490f9f5a03f701cbc78301d4c02db7a791cca8
palomaprates <paloma.prates108@gmail.com>
2025-07-24 09:01:45 +0100

    att layout products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,47 +1,36 @@
-import anelsize from '../assets/anelsize.png'
-import pulseiraolho from '../assets/pulseirasolho.png'
-import stoantonio from '../assets/stoantonio.png'
-type ProductData = {
-    title: string;
-    image: string;
-    description: string;
-};
+import anelsize from "../assets/anelsize.png";
+import pulseiraolho from "../assets/pulseirasolho.png";
+import stoantonio from "../assets/stoantonio.png";
 
-export default function Products()
-{
-    const products : ProductData[] = [
-        {
-            title: 'Filigranas',
-            image: stoantonio,
-            description: 'Filigrana Patrono Lisboa', 
-        },
-        {
-            title: 'Jóias em Aço',
-            image: anelsize,
-            description: 'Anel em aço banhado a ouro', 
-        },
-        {
-            title: 'Jóias artesanais',
-            image: pulseiraolho,
-            description: 'pulseira.', 
-        },
-
-    ]
-    return(
-
-        <div className=''>
-        <div className='grid grid-cols-1 justify-center md:grid-cols-3 gap-32 p-7'>
-        {products.map((product, index) =>
-        <div key={index}
-        className='overflow-hidden transform transition duration-300 hover:scale-105 w-80 rounded-1xl'>
-        <img src={product.image} alt={product.title} className='w-full h-full object-cover' />
-        <div className='pt-2'>
-        <p className="text-lg leading-relaxed font-medium flex justify-center">{product.title}</p>    
-        </div>    
-        </div>
-        )}
-        </div>    
-        </div>
-        );
+export default function Products() {
+  const products = [
+    {
+      image: anelsize,
+      title: "Anel Size",
+    },
+    {
+      image: pulseiraolho,
+      title: "Pulseira Olho",
+    },
+  ];
+  return (
+    <div className="flex flex-row justify-center">
+      <img
+        src={stoantonio}
+        alt="Sto Antonio"
+        className="w-1/2 h-full object-cover"
+      />
+      <div className="grid grid-cols-1 justify-center md:grid-cols-2 md:flex md:flex-col">
+        {products.map((product, index) => (
+          <div key={index} className="overflow-hidden w-80 h-1/2">
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full h-6/5 object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
