f14d4a9ff8045acf8b53cd62c8fab3e7b2d28c19
palomaprates <paloma.prates108@gmail.com>
2025-07-24 12:26:04 +0100

    finish sobreNos component

--- src/components/SobreNos.tsx
+++ src/components/SobreNos.tsx
@@ -1,27 +1,56 @@
+import colarpeixeazul from "../assets/colarpeixeazul.png";
+import coracaovianalila from "../assets/coracaovianalila.png";
+import pulseiratartaruga from "../assets/pulseiratartaruga.png";
+
 export default function SobreNos() {
   return (
-    <section className="py-16 px-6 text-gray-800">
-      <div className="max-w-full mx-auto text-center space-y-6">
-        <p className="text-lg leading-relaxed">
-          Na <span className="font-semibold">Lila Acessórios</span>, cada peça é cuidadosamente <span className="font-semibold">feita à mão</span>, garantindo exclusividade, beleza e significado.
+    <section className="py-16 px-16 text-gray-800">
+      <div className=" w-full flex gap-5 flex-col justify-center">
+        <div className="grid md:grid-cols-2 md:px-10 mx-10 md:gap-7 gap-1 shadow-md">
+          <img
+            src={colarpeixeazul}
+            alt="Colar Peixe Azul"
+            className="w-120 h-120 object-cover"
+          />
+          <p className="text-xl leading-relaxed flex wrap-normal items-center">
+            Na Lila Acessórios, cada peça é cuidadosamente feita à mão,
+            garantindo exclusividade, beleza e significado.
+          </p>
+        </div>
+        <div className="grid md:grid-cols-2 md:px-10 mx-10 gap-1 shadow-md">
+          <img
+            src={pulseiratartaruga}
+            alt="Pulseira Tartaruga"
+            className="w-120 h-120 object-cover"
+          />
+          <p className="text-xl leading-relaxed flex wrap-normal items-center">
+            Os nossos produtos são resistentes à água, perfeitos para quem vive
+            intensamente — seja na praia, piscina ou no dia-a-dia.
+          </p>
+        </div>
+
+        <div className="grid md:grid-cols-2 md:px-10 mx-10 gap-1 shadow-md">
+          <img
+            src={coracaovianalila}
+            alt="Colar Peixe Azul"
+            className="w-120 h-120 object-cover"
+          />
+          <p className="text-xl leading-relaxed flex wrap-normal items-center">
+            Também valorizamos as tradições portuguesas através da filigrana,
+            uma arte milenar que transforma finos fios de ouro ou prata em obras
+            delicadas e simbólicas. Esta técnica, com raízes profundas na
+            cultura portuguesa, representa elegância e herança.
+          </p>
+        </div>
+
+        {/* <p className="text-xl font-medium" style={{ color: "#00A99D" }}>
+          Mais do que acessórios, oferecemos autenticidade e histórias que tu
+          carregas contigo.
         </p>
 
-        <p className="text-lg leading-relaxed">
-          Os nossos produtos são <span className="font-semibold">resistentes à água</span>, perfeitos para quem vive intensamente — seja na <span className="italic">praia, piscina</span> ou no dia-a-dia.
-        </p>
-
-        <p className="text-lg leading-relaxed">
-          Também valorizamos as tradições portuguesas através da <span className="font-semibold">filigrana</span>, uma arte milenar que transforma finos fios de ouro ou prata em obras delicadas e simbólicas. 
-          Esta técnica, com raízes profundas na cultura portuguesa, representa elegância e herança.
-        </p>
-
-        <p className="text-xl font-medium" style={{ color: '#00A99D' }}>
-          Mais do que acessórios, oferecemos autenticidade e histórias que tu carregas contigo.
-        </p>
-
-        <p className="text-xl font-pacifico" style={{ color: '#00A99D' }}>
-          Bem-vindos à lila 
-        </p>
+        <p className="text-xl font-pacifico" style={{ color: "#00A99D" }}>
+          Bem-vindos à lila
+        </p> */}
       </div>
     </section>
   );
